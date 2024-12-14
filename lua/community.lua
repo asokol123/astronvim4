4bb53ca96713f275b71662692234631bbca50f14
Aleksei Sokolovskii <asokolovskii@bhft.com>
2024-12-14 16:53:52 +0500

    save

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
   -- import/override with your plugins folder
 }
--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -41,11 +41,12 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "clangd",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
